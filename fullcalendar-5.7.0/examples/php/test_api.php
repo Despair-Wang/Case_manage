7eddb2846f4073b3c875befed32885916cf0f536
Despair Wang <mkazuya2810@gmail.com>
2021-06-16 17:11:23 +0800

    移除FC DEMO

--- fullcalendar-5.7.0/examples/php/test_api.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "db_cone.php";
-$sql = "select `title`,`start` from `schedule` where 1;";
-$result = mysqli_query($con, $sql);
-$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-// print_r($rows);
-echo $rows;
